58713ff449eeb45c3ad1526a090d8635f1207a69
Nerver4Ever <Nerver4Ever@outlook.com>
2022-07-30 14:17:03 +0800

    * update “根目录显示乱码” * add aot

--- Program.cs
+++ Program.cs
@@ -8,10 +8,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMasaBlazor();
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton(_ => new Manager("¸ùÄ¿Â¼"));
 builder.Services.AddSingleton<VersionInfo>();
-
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 //builder.Services.AddSingleton<WeatherForecastService>();
 
 await builder.Build().RunAsync();
